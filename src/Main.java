37cf52fc24e0ec9e0409c43d8dd07c0178c2e88c
KarixD2137 <karol.kopczynski7@gmail.com>
2024-09-04 15:45:12 +0200

    dodatkowy komunikat

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     // Nazwa klas powinny być z wielkiej litery
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Witam serdecznie!");
     }
 }
