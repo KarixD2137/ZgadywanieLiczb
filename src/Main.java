651f3cda67ba5b21027185271c6f0ea4bc515920
KarixD2137 <karol.kopczynski7@gmail.com>
2024-09-04 15:58:24 +0200

    losowanie liczby rzeczywistej

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
+/*
+git init
+git config --local user.name "KarixD2137"
+git config --local user.email "karol.kopczynski7@gmail.com"
+git add .
+git commit -m "pusty projekt"
+git log
+git remote add origin https://github.com/KarixD2137/ZgadywanieLiczb.git
+git push -u origin master
+*/
+
 public class Main {
     // Nazwa pliku i nazwa klasy muszą być takie same
     // Nazwa klas powinny być z wielkiej litery
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Witam serdecznie!");
+
+        // Typ prosty - tylko wartość, pisany z małej litery
+        // Typ złożony - objekt zawierający własności i metody, pisany z dużej litery
+
+        double wylosowanaLiczba = Math.random()*10+1;
+        System.out.println(wylosowanaLiczba);
     }
 }
